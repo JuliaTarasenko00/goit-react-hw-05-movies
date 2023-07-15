7349a18f7fc523f8065488fcd39df3da27269a5e
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-07-15 19:09:32 +0300

    hw5

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,8 +1,10 @@
+import ErrorNotFound from 'imgError/ErrorNotFound.png';
+
 const NotFound = () => {
   return (
     <img
       style={{ margin: 'auto', marginTop: '50px' }}
-      src="https://img.freepik.com/free-vector/404-error-with-a-cute-animal-concept-illustration_114360-5528.jpg"
+      src={ErrorNotFound}
       alt="Error"
       width="500"
       loading="lazy"
